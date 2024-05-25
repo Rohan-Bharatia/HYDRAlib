b483d7a8d571d64a7d7b0f4252e44e52102e8713
Rohan Bharatia <167599567+Rohan-Bharatia@users.noreply.github.com>
2024-05-25 22:08:57 +0000

    Add Main Header

---
+++ HYDRAlib.h
@@ -0,0 +1,69 @@
+#pragma region LICENSE
+
+// MIT License
+
+// Copyright (c) 2024 Rohan Bharatia
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#pragma endregion LICENSE
+
+#pragma once
+
+#ifndef HYDRAlib_H
+#define HYDRAlib_H
+
+namespace HYDRAlib
+{
+    static constexpr int HYDRAlib_VERSION_MAJOR = 1;
+    static constexpr int HYDRAlib_VERSION_MINOR = 0;
+    static constexpr int HYDRAlib_VERSION_PATCH = 0;
+
+    static constexpr int PORT_1 = 1;
+    static constexpr int PORT_2 = 2;
+    static constexpr int PORT_3 = 3;
+    static constexpr int PORT_4 = 4;
+    static constexpr int PORT_5 = 5;
+    static constexpr int PORT_6 = 6;
+    static constexpr int PORT_7 = 7;
+    static constexpr int PORT_8 = 8;
+    static constexpr int PORT_9 = 9;
+    static constexpr int PORT_10 = 10;
+    static constexpr int PORT_11 = 11;
+    static constexpr int PORT_12 = 12;
+    static constexpr int PORT_13 = 13;
+    static constexpr int PORT_14 = 14;
+    static constexpr int PORT_15 = 15;
+    static constexpr int PORT_16 = 16;
+    static constexpr int PORT_17 = 17;
+    static constexpr int PORT_18 = 18;
+    static constexpr int PORT_19 = 19;
+    static constexpr int PORT_20 = 20;
+
+    static constexpr char PORT_A = 'A';
+    static constexpr char PORT_B = 'B';
+    static constexpr char PORT_C = 'C';
+    static constexpr char PORT_D = 'D';
+    static constexpr char PORT_E = 'E';
+    static constexpr char PORT_F = 'F';
+    static constexpr char PORT_G = 'G';
+    static constexpr char PORT_H = 'H';
+} // namespace HYDRAlib
+
+#endif
