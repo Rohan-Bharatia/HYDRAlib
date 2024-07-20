2866d51f2d828e85f8c7bf3ef1e20ebd0c8f3b38
Rohan Bharatia <167599567+Rohan-Bharatia@users.noreply.github.com>
2024-07-20 17:03:39 -0400

    Add Macros

--- HYDRAlib.h
+++ HYDRAlib.h
@@ -20,16 +20,7 @@
 #include "include/api.h"
 #include "include/main.h"
 #include "include/autons.hpp"
-
-#define motor_clamp(max)                                                                              \
-    do                                                                                                \
-    {                                                                                                 \
-        if(Priv::motor_count > max)                                                                   \
-        {                                                                                             \
-            printf("Your robot has exceeded the maximum legal wattage (%i Watts) allowed", max * 11); \
-            running = false;                                                                          \
-        }                                                                                             \
-    } while(0)
+#include "include/macros.h"
 
 #ifdef _V5RC
 
@@ -54,36 +45,6 @@ namespace HYDRAlib
     static constexpr uint8_t HYDRAlib_VERSION_MAJOR = 1;
     static constexpr uint8_t HYDRAlib_VERSION_MINOR = 0;
     static constexpr uint8_t HYDRAlib_VERSION_PATCH = 0;
-
-    static constexpr uint8_t PORT_1  = 1;
-    static constexpr uint8_t PORT_2  = 2;
-    static constexpr uint8_t PORT_3  = 3;
-    static constexpr uint8_t PORT_4  = 4;
-    static constexpr uint8_t PORT_5  = 5;
-    static constexpr uint8_t PORT_6  = 6;
-    static constexpr uint8_t PORT_7  = 7;
-    static constexpr uint8_t PORT_8  = 8;
-    static constexpr uint8_t PORT_9  = 9;
-    static constexpr uint8_t PORT_10 = 10;
-    static constexpr uint8_t PORT_11 = 11;
-    static constexpr uint8_t PORT_12 = 12;
-    static constexpr uint8_t PORT_13 = 13;
-    static constexpr uint8_t PORT_14 = 14;
-    static constexpr uint8_t PORT_15 = 15;
-    static constexpr uint8_t PORT_16 = 16;
-    static constexpr uint8_t PORT_17 = 17;
-    static constexpr uint8_t PORT_18 = 18;
-    static constexpr uint8_t PORT_19 = 19;
-    static constexpr uint8_t PORT_20 = 20;
-
-    static constexpr char PORT_A = 'A';
-    static constexpr char PORT_B = 'B';
-    static constexpr char PORT_C = 'C';
-    static constexpr char PORT_D = 'D';
-    static constexpr char PORT_E = 'E';
-    static constexpr char PORT_F = 'F';
-    static constexpr char PORT_G = 'G';
-    static constexpr char PORT_H = 'H';
 } // namespace HYDRAlib
 
 #endif // _HYDRAlib_h_
---
+++ include/macros.h
@@ -0,0 +1,118 @@
+#pragma region LICENSE
+
+//                    GNU GENERAL PUBLIC LICENSE
+//                       Version 3, 29 June 2007
+// 
+// Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
+// Everyone is permitted to copy and distribute verbatim copies
+// of this license document, but changing it is not allowed.
+//                                 ...                                
+// 
+//                  Copyright (c) Rohan Bharatia 2024
+
+#pragma endregion LICENSE
+
+#pragma once
+
+#ifndef _HYDRAlib_MACROS_h_
+#define _HYDRAlib_MACROS_h_
+
+// std
+#include <iostream>
+#include <chrono>
+
+#define HYDRAlib_VERSION_MAJOR = 1
+#define HYDRAlib_VERSION_MINOR = 0
+#define HYDRAlib_VERSION_PATCH = 0
+
+#define PORT_1  01
+#define PORT_2  02
+#define PORT_3  03
+#define PORT_4  04
+#define PORT_5  05
+#define PORT_6  06
+#define PORT_7  07
+#define PORT_8  08
+#define PORT_9  09
+#define PORT_10 10
+#define PORT_11 11
+#define PORT_12 12
+#define PORT_13 13
+#define PORT_14 14
+#define PORT_15 15
+#define PORT_16 16
+#define PORT_17 17
+#define PORT_18 18
+#define PORT_19 19
+#define PORT_20 20
+
+#define PORT_A 'A'
+#define PORT_B 'B'
+#define PORT_C 'C'
+#define PORT_D 'D'
+#define PORT_E 'E'
+#define PORT_F 'F'
+#define PORT_G 'G'
+#define PORT_H 'H'
+
+#define motor_clamp(max)                                                                              \
+    do                                                                                                \
+    {                                                                                                 \
+        if(Priv::motor_count > max)                                                                   \
+        {                                                                                             \
+            printf("Your robot has exceeded the maximum legal wattage (%i Watts) allowed", max * 11); \
+            running = false;                                                                          \
+        }                                                                                             \
+    } while(0)
+
+#ifdef !NDEBUG
+
+#define DEBUG(msg) std::cerr << "[DEBUG]: " << msg << std::endl
+
+#else
+
+#define DEBUG(msg)
+
+#endif // !NDEBUG
+
+#define INFO(msg)    std::cout << "[INFO]: " << msg << std::endl
+#define WARNING(msg) std::cerr << "[WARNING]: " << msg << std::endl
+#define ERROR(msg)   std::cerr << "[ERROR]: " << msg << std::endl
+
+#define SAFE_DELETE(ptr)   \
+    do                     \
+    {                      \
+        if(ptr)            \
+        {                  \
+            delete ptr;    \
+            ptr = nullptr; \
+        }                  \
+    } while(0)
+
+#define SAFE_DELETE_ARRAY(ptr)   \
+    do                           \
+    {                            \
+        if(ptr)                  \
+        {                        \
+            delete[] ptr;        \
+            ptr = nullptr;       \
+        }                        \
+    } while(0)
+
+#define CONCATENATE(arg1, arg2) arg1##arg2
+
+#define SCOPE_EXIT(code)                                      \
+    auto CONCATENATE(scope_exit_, __LINE__) = [&]() { code; }
+
+#define SET_BIT(value, bit)    ((value) |= (1UL << (bit)))
+#define CLEAR_BIT(value, bit)  ((value) &= ~(1UL << (bit)))
+#define TOGGLE_BIT(value, bit) ((value) ^= (1UL << (bit)))
+#define CHECK_BIT(value, bit)  ((value) & (1UL << (bit)))
+
+#define UNUSED(x) (void)(x)
+
+#define START_TIMER  auto start = std::chrono::high_resolution_clock::now()
+#define STOP_TIMER   auto stop = std::chrono::high_resolution_clock::now()
+#define ELAPSED_TIME std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count()
+
+#endif // _HYDRAlib_MACROS_h_
